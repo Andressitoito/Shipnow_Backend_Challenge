5865e1dc4fbd1ff27a0ea45d6c0bb370d13e8ff6
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-06-27 22:38:22 -0300

    Console startup and close
    The console is ready to ask and receive commands from the user. At the moment, the quit command to exit and close de program, is ready.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
+/////////////////////////////
+// VARIABLES & REQUIRES
+/////////////////////////////
+const readline = require("readline");
 
+/////////////////////////////
+// MAIN CONSOLE LOGIC
+/////////////////////////////
+function startConsole() {
+	// Setup console to read and write to the actual console
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+	function promptCommand() {
+		// Ask a command to the user
+		rl.question("Write a command > ", (input) => {
+			// Pass to runCommand the input inserted by the user
+			runCommand(input);
+
+			// Use resursion to ask for another command
+			promptCommand();
+		});
+	}
+	promptCommand();
+}
+
+/////////////////////////////
+// MAIN COMMAND FUNCTION
+/////////////////////////////
+function runCommand(input) {
+	console.log("Inserted command:", input);
+
+	if (input === "quit") {
+		quit();
+	}
+}
+
+/////////////////////////////
+// INTERACTIVE FUNCTIONS
+/////////////////////////////
+function quit() {
+	console.log("Exiting command prompt");
+	process.exit(0);
+}
+
+/////////////////////////////
+// START THE CONSOLE
+/////////////////////////////
+startConsole();
