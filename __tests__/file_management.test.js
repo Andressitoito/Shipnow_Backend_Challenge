c0e460e1f71f1b334581b5b429a9b3812de57daf
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-07-10 18:58:30 -0300

    Implement Error Handling for 'create_file' Test
    The test has been executed and verified, resulting in a failed response as expected.

--- __tests__/file_management.test.js
+++ __tests__/file_management.test.js
@@ -26,4 +26,23 @@ describe("create_file", () => {
 		expect(fileSystem.currentDirectory.children[0].type).toBe("file");
 		expect(fileSystem.currentDirectory.children[0].content).toBe(content);
 	});
+
+	test("should not create a file with an existing name", () => {
+		// Arrange
+		const fileSystem = {
+			currentDirectory: {
+				children: [{ name: "existing.txt", type: "file" }],
+			},
+		};
+		const fileName = "existing.txt";
+		const content = "New content";
+
+		// Act
+		create_file(fileSystem, fileName, content);
+
+		// Assert
+		expect(fileSystem.currentDirectory.children).toHaveLength(1);
+		// The existing file should not be overwritten
+		expect(fileSystem.currentDirectory.children[0].content).not.toBe(content);
+	});
 });
